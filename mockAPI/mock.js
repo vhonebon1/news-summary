e040d64dcf3c0c17cd9d20212df7d3844af6dc2a
Verity <vhonebon@gmail.com>
2017-07-23 00:50:11 +0100

    Added mock API response

---
+++ mockAPI/mock.js
@@ -0,0 +1,18 @@
+module.exports = [{
+  {
+    "response": {
+      "status": "ok",
+      "userTier": "free",
+      "total": 1,
+      "content": {
+        "id": "business/2014/feb/18/uk-inflation-falls-below-bank-england-target",
+        "sectionId": "business",
+        "sectionName": "Business",
+        "webPublicationDate": "2014-02-18T11:02:45Z",
+        "webTitle": "UK inflation falls below Bank of England's 2% target",
+        "webUrl": "https://www.theguardian.com/business/2014/feb/18/uk-inflation-falls-below-bank-england-target",
+        "apiUrl": "https://content.guardianapis.com/business/2014/feb/18/uk-inflation-falls-below-bank-england-target"
+      }
+    }
+  }
+}];
