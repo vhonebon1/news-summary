639eed5ec1c2ccb1b0c43fc0f3e70a52ee31309d
Verity <vhonebon@gmail.com>
2017-07-22 16:17:27 +0100

    tests refactored

--- test/unit/news-story-list-model-spec.js
+++ test/unit/news-story-list-model-spec.js
@@ -1,28 +1,19 @@
+(function() {
 
-function testStartsWithEmptyArray(){
-   var list = new NewsStoryList()
-   assert.isTrue(list.stories.length === 0)
-}
-
-testStartsWithEmptyArray();
+var list = new NewsStoryList()
+var newsStory = new NewsStory("first headline", "first content")
+var newsStory2 = new NewsStory("second headline", "second content")
+list.pushToStories(newsStory);
+list.pushToStories(newsStory2);
 
 function testPushToStories() {
-  var list = new NewsStoryList()
-  var newsStory = new NewsStory("test headline", "test content")
-  list.pushToStories(newsStory);
-  assert.isTrue(list.stories.length === 1);
+  assert.isTrue(list.stories.length === 2);
   assert.isTrue(list.stories[0].id === 0);
 };
 
 testPushToStories();
 
 function testGetHeadlines() {
-  var list = new NewsStoryList()
-  var newsStory = new NewsStory("first headline", "first content")
-  var newsStory2 = new NewsStory("second headline", "second content")
-  list.pushToStories(newsStory);
-  list.pushToStories(newsStory2);
-  console.log(list.getHeadlines())
   assert.isTrue(list.getHeadlines() === "first headlinesecond headline");
   assert.isTrue(list.stories[0].id === 0);
 };
@@ -30,23 +21,14 @@ function testGetHeadlines() {
 testGetHeadlines();
 
 function testGetHeadlines() {
-  var list = new NewsStoryList()
-  var newsStory = new NewsStory("first headline", "first content")
-  var newsStory2 = new NewsStory("second headline", "second content")
-  list.pushToStories(newsStory);
-  list.pushToStories(newsStory2);
   assert.isTrue(list.getHeadlines() === "first headlinesecond headline");
 };
 
 testGetHeadlines();
 
 function testStoryContents() {
-  var list = new NewsStoryList()
-  var newsStory = new NewsStory("first headline", "first content")
-  var newsStory2 = new NewsStory("second headline", "second content")
-  list.pushToStories(newsStory);
-  list.pushToStories(newsStory2);
   assert.isTrue(list.getStoryContents() === "first contentsecond content");
 };
 
 testGetHeadlines();
+})();
--- test/unit/news-story-model-spec.js
+++ test/unit/news-story-model-spec.js
@@ -1,41 +1,40 @@
+(function() {
+
+var news = new NewsStory("Some headline", "Some content");
+
 function testHasNewsStory(){
-  var news = new NewsStory("Edward Snowden: the whistleblower behind the NSA surveillance revelations", "Some story about Edward Snowden");
-  assert.isTrue(news.storyContent === "Some story about Edward Snowden")
+  assert.isTrue(news.storyContent === "Some content")
 }
 
 testHasNewsStory();
 
 function testHasNewsHeadline(){
-  var news = new NewsStory("Edward Snowden: the whistleblower behind the NSA surveillance revelations", "Some story about Edward Snowden");
-  assert.isTrue(news.headline === "Edward Snowden: the whistleblower behind the NSA surveillance revelations")
+  assert.isTrue(news.headline === "Some headline")
 }
 
 testHasNewsHeadline();
 
 function testStartsWithNullId(){
-  var news = new NewsStory("Edward Snowden: the whistleblower behind the NSA surveillance revelations", "Some story about Edward Snowden");
   assert.isTrue(news.id === null)
 }
 
 testStartsWithNullId();
 
 function testGetHeadlineWorks(){
-  var news = new NewsStory("Edward Snowden: the whistleblower behind the NSA surveillance revelations", "Some story about Edward Snowden");
-  assert.isTrue(news.getHeadline() === "Edward Snowden: the whistleblower behind the NSA surveillance revelations")
+  assert.isTrue(news.getHeadline() === "Some headline")
 }
 
 testGetHeadlineWorks();
 
 function testGetStoryWorks(){
-  var news = new NewsStory("Edward Snowden: the whistleblower behind the NSA surveillance revelations", "Some story about Edward Snowden");
-  assert.isTrue(news.getStory() === "Some story about Edward Snowden")
+  assert.isTrue(news.getStory() === "Some content")
 }
 
 testGetStoryWorks();
 
 function testGetIdWorks(){
-  var news = new NewsStory("Edward Snowden: the whistleblower behind the NSA surveillance revelations", "Some story about Edward Snowden");
   assert.isTrue(news.getId() === null)
 }
 
 testGetIdWorks();
+})();
