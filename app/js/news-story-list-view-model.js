cc2d32221a4d78b2010aa0730926027eb9760310
Verity <vhonebon@gmail.com>
2017-07-22 19:02:52 +0100

    Added news story list view model and tests

---
+++ app/js/news-story-list-view-model.js
@@ -0,0 +1,25 @@
+(function(exports) {
+  function NewsStoryListView(newsStoryList) {
+    this.newsStoryList = newsStoryList;
+  };
+
+  NewsStoryListView.prototype.convertToHtml = function() {
+    if (this.newsStoryList.getHeadlines().length >= 1) {
+      this._addInnerHtmlTags();
+    };
+  }
+
+  NewsStoryListView.prototype._addInnerHtmlTags = function() {
+    var storiesHtml = []
+    this.newsStoryList.getStories().map(function(story){
+      storiesHtml.push(_addTagsForOneNote(story))
+    });
+    return storiesHtml;
+  }
+
+  function _addTagsForOneStory(note) {
+    return "<div><a href=" + note.getHeadline() + "</a></div><br>"
+  };
+
+  exports.NewsStoryListView = NewsStoryListView;
+})(this);
---
+++ test/unit/news-story-list-view-spec.js
@@ -0,0 +1,19 @@
+(function() {
+
+  function testViewStartsWithNewsStoryList() {
+    story = new NewsStory("Alarming Headline", "Alarming text")
+    list = new NewsStoryList(story);
+    view = new NewsStoryListView(list)
+    list.pushToStories()
+    assert.isTrue(view.newsStoryList === list);
+  };
+
+  testViewStartsWithNewsStoryList();
+
+  function testAddToPage() {
+
+    // mock adding headlines here
+
+    testAddToPage();
+
+})();
