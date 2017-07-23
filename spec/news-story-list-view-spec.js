e13991aa93d996774deec39aa2d17ba3f1934fab
Verity <vhonebon@gmail.com>
2017-07-23 15:29:36 +0100

    Added extra test for multiples

--- spec/news-story-list-view-spec.js
+++ spec/news-story-list-view-spec.js
@@ -1,19 +1,22 @@
 (function() {
+  firstStory = new NewsStory("Alarming Headline", "Alarming text")
+  secondStory = new NewsStory("Calming Headline", "Calming text")
+  list = new NewsStoryList();
+  list.pushToStories(firstStory);
+  list.pushToStories(secondStory);
+  view = new NewsStoryListView(list)
 
   function testViewStartsWithNewsStoryList() {
-    story = new NewsStory("Alarming Headline", "Alarming text")
-    list = new NewsStoryList(story);
-    list.pushToStories(story)
-    view = new NewsStoryListView(list)
     assert.isTrue(view.newsStoryList === list);
   };
 
   testViewStartsWithNewsStoryList();
 
   function testConvertToHTML() {
-    assert.isEquals(view.convertToHtml(), "<ol><li><div><a href=\"#stories/0\">Alarming Headline</a></div></li></ol>")
+    assert.isEquals(view.convertToHtml(), "<ol><li><div><a href=\"#stories/0\">Alarming Headline</a></div></li><li><div><a href=\"#stories/1\">Calming Headline</a></div></li></ol>")
   }
 
   testConvertToHTML();
+
 
 })();
